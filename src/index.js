8842f98889f323aa8114cb432a20d32fd0c4c316
hoangvuong.tran21 <hoangvuong.tran21@gmail.com>
2021-02-17 21:48:03 +0700

    Setup babel and source-map

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const newObject = {
+  test: 1,
+  kaka: 2,
+  play: 3
+}
+
+const otherObject = {
+  ...newObject,
+  guitar: 22
+}
+
+console.log(newObject);
+
+console.log('nice hot reloading', otherObject);
